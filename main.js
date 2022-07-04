17750ae6e5b2abfe1ddabef04574d72ffe82d1cd
wewireaca <101429066+wewireaca@users.noreply.github.com>
2022-07-04 14:46:27 -0500

    make it better

---
+++ main.js
@@ -0,0 +1,48 @@
+const PUBLIC_VAPID_KEY =
+  "BNBZYTse3GG6kodGXolZi1BsjESZV0dP4TmtcJXCSrs2bD7OvM1CX9j6R-GpjP76qY7WZ5UU5ksXd_m74s_i3rA";
+
+const subscription = async () => {
+  // Service Worker
+  console.log("Registering a Service worker");
+  const register = await navigator.serviceWorker.register("/worker.js", {
+    scope: "/"
+  });
+  console.log("New Service Worker");
+
+  // Listen Push Notifications
+  console.log("Listening Push Notifications");
+  const subscription = await register.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: urlBase64ToUint8Array(PUBLIC_VAPID_KEY)
+  });
+
+  console.log(subscription);
+
+  // Send Notification
+  await fetch("/subscription", {
+    method: "POST",
+    body: JSON.stringify(subscription),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+  console.log("Subscribed!");
+};
+
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+// Service Worker Support
+if ("serviceWorker" in navigator) {
+  subscription().catch(err => console.log("------> " + err));
+}
---
+++ sw.js
@@ -0,0 +1,49 @@
+self.addEventListener('install', function (event) {
+  console.log('SW Installed');
+  event.waitUntil(
+    caches.open('static')
+      .then(function (cache) {
+        cache.add('/');
+        cache.add('/index.html');
+        cache.add('/src/js/app.js');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/src/js/app.js',
+          '/src/css/app.css',
+          '/src/images/pwa.jpg',
+          '/src/images/icon-512x512.png',
+          'https://fonts.googleapis.com/css?family=Raleway:400,700'
+        ]);
+      })
+  );
+});
+
+self.addEventListener('activate', function () {
+  console.log('SW Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) {
+          return res;
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
+});
+
+self.addEventListener('push', e => {
+  const data = e.data.json();
+  console.log(data)
+  console.log('Notification Received');
+  self.registration.showNotification(data.title, {
+      body: data.message,
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Archlinux-icon-crystal-64.svg/1024px-Archlinux-icon-crystal-64.svg.png'
+  });
+});
+
+
