9b787ba581f4d8eec46bafa5bbfdea51d2946c14
MarcosTapia <marcostapia623@gmail.com>
2022-07-05 12:11:59 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -18,10 +18,15 @@ self.addEventListener('activate', function () {
 });
 
 
-self.addEventListener('push', function (e) {
-  console.log('SW Activated');
+self.addEventListener('push', e => {
+  const data = e.data.json();
+  console.log(data)
+  console.log('Notification Received');
+  self.registration.showNotification(data.title, {
+      body: data.message,
+      icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Archlinux-icon-crystal-64.svg/1024px-Archlinux-icon-crystal-64.svg.png'
+  });
 });
-
 
 
 
