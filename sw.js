44a4ddb6f412e363f68092c1c699a3d855880415
MarcosTapia <marcostapia623@gmail.com>
2022-07-05 12:05:44 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,8 +5,7 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open('static')
       .then(function (cache) {
-        cache.add('index.html');
-        
+        cache.add('index.html');      
         
         
       })
@@ -15,21 +14,7 @@ self.addEventListener('install', function (event) {
 
 
 self.addEventListener('activate', function () {
-
   console.log('SW Activated');
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(res) {
-        if (res) {
-          return res;
-        } else {
-          return fetch(event.request);
-        }
-      })
-  );
 });
 
 
