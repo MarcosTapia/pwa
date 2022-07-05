82c5426f1a5acc2247f25b9c8b328f51a6332a26
MarcosTapia <marcostapia623@gmail.com>
2022-07-05 08:51:31 -0500

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,3 @@
-var pwaCard = document.querySelector('#pwa');
-var pwaCardContent = pwaCard.querySelector('.card__content');
-var pwaCardDetails = pwaCard.querySelector('.card__details');
-var detailsShown = false;
-
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js')
     .then(function() {
